95a25de71aac0ca00fc1965a5cf8d3106e47cd3f
sulenq <whyimustwriteit@gmail.com>
2023-07-20 14:13:37 +0700

    first

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import "./App.css";
+
+function App() {
+  const gender: string = "Getting set up anjay";
+
+  return <div>{gender}</div>;
+}
+
+export default App;
